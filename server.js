75f4dca2cf97429d15d3e8112ed132184d412852
Ansari Afroz Ahmed <ansariafroz.py@gmail.com>
2023-12-23 21:34:13 +0530

    SignUp & SignIn DONE

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const pool = require("./config/db");
+const bcrypt = require("bcrypt");
 
 const app = express();
 
@@ -10,28 +11,71 @@ app.use(cors());
 app.listen(4000, () => console.log("SERVER RUNNING ON 4000"));
 
 //SignUp Process API
-app.post("/api/signup", (req, res) => {
+app.post("/api/signup", async (req, res) => {
   const name = req.body["fullName"];
   const email = req.body["email"];
   const password = req.body["password"];
-
-  // console.log(name);
-  // console.log(email);
-  // console.log(password);
-
-  const adduserquery = `INSERT INTO vwuser (vw_name,vw_email,vw_password) VALUES ('${name}','${email}','${password}');`;
-
+  //Hashing The Password
+  const hashedPassword = await bcrypt.hash(password, 10);
+  //Query
+  const adduserquery = `INSERT INTO vwuser (vw_name,vw_email,vw_password) VALUES ('${name}','${email}','${hashedPassword}');`;
+  //Firing Query
   pool
     .query(adduserquery)
     .then((response) => {
       console.log("DATA ADDED");
       // console.log(response);
+      res.status(200).send("Data added successfully");
     })
     .catch((error) => {
       console.log("DATA NOT ADDED");
-      // console.log(error);
-    });
+      console.error(error);
 
-  console.log(req.body);
-  res.send("Response Received" + req.body);
+      if (error.code === "ER_DUP_ENTRY") {
+        // Duplicate entry error (assuming email is a unique key)
+        res.status(400).send("Email already exists");
+      } else {
+        // Generic error response
+        res.status(500).send("Internal Server Error");
+      }
+    });
+  // console.log(req.body);
+  // res.send("Response Received" + req.body);
+});
+
+// Login Process API
+app.post("/api/signin", async (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  try {
+    const getUserQuery = "SELECT * FROM vwuser WHERE vw_email = $1";
+    const user = await pool.query(getUserQuery, [email]);
+
+    if (user.rows.length > 0) {
+      const isPasswordValid = await bcrypt.compare(
+        password,
+        user.rows[0].vw_password
+      );
+
+      if (isPasswordValid) {
+        // Password is valid, send user data (excluding the password) in the response
+        const userData = {
+          user_id: user.rows[0].user_id,
+          vw_email: user.rows[0].vw_email,
+          vw_name: user.rows[0].vw_name,
+        };
+        res.status(200).json(userData);
+      } else {
+        // Password is invalid
+        res.status(401).send("Invalid Password");
+      }
+    } else {
+      // User not found
+      res.status(404).send("User Not Found");
+    }
+  } catch (error) {
+    console.error("Error:", error);
+    res.status(500).send("Internal Server Error");
+  }
 });
